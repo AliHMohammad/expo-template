ab0e589d48f50c96029fee843e530edc43960185
Ali Mohammad <alihmohammad@hotmail.com>
2024-09-01 15:18:56 +0200

    feat: now possible to add new Restaurants

--- app/(tabs)/restaurants/index.tsx
+++ app/(tabs)/restaurants/index.tsx
@@ -1,8 +1,26 @@
-import { View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
 import { useRouter } from "expo-router";
 import { RestaurantCard } from "../../../components/RestaurantCard";
+import React from "react";
+
+const INIT_DATA = [
+    {
+        id: 0,
+        name: "KFC",
+    },
+    {
+        id: 1,
+        name: "Riz Raz",
+    },
+    {
+        id: 2,
+        name: "Stefanos",
+    },
+];
 
 export default function RestaurantsPage() {
+    const [restaurants, setRestaurants] = React.useState([...INIT_DATA]);
+    const [input, setInput] = React.useState("");
     const router = useRouter();
 
     const handlePress = (name: string) => {
@@ -14,12 +32,24 @@ export default function RestaurantsPage() {
         router.push(`/restaurants/${name}`);
     };
 
+    const handleCreate = () => {
+        if (input === "") return;
+
+        setRestaurants((prev) => [...prev, { id: prev.length, name: input }]);
+        setInput("");
+    };
+
     return (
-        <View>
-            <View className="gap-y-2 mt-2">
-                <RestaurantCard className="mx-2" name="KFC" onPress={() => handlePress("KFC")} />
-                <RestaurantCard className="mx-2" name="Riz Raz" onPress={() => handlePress("Riz Raz")} />
+        <ScrollView>
+            <View>
+                <Text className="pt-3 text-center">Create a new restaurant</Text>
+                <TextInput returnKeyType="done" className="border p-3 m-3" value={input} onChangeText={setInput} onEndEditing={handleCreate} />
             </View>
-        </View>
+            <View className="gap-y-2 mt-2">
+                {restaurants.map((r) => (
+                    <RestaurantCard key={r.id} className="mx-2" name={r.name} onPress={handlePress} />
+                ))}
+            </View>
+        </ScrollView>
     );
 }

c7a1c96dddf308eba71dd433850eeae8b01ad91f
Ali Mohammad <alihmohammad@hotmail.com>
2024-09-16 22:38:05 +0200

    feat: created FirebaseClient and Restaurant Endpoint

--- app/(tabs)/restaurants/index.tsx
+++ app/(tabs)/restaurants/index.tsx
@@ -1,43 +1,28 @@
-import { Button, ScrollView, View } from "react-native";
+import { Button, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { RestaurantCard } from "../../../components/RestaurantCard";
 import React, { useEffect } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { database } from "../../../firebase";
-
 import { useCollection } from "react-firebase-hooks/firestore";
+import IRestaurant from "../../../models/Restaurant";
 
-const INIT_DATA = [
-    {
-        id: 0,
-        name: "KFC",
-    },
-    {
-        id: 1,
-        name: "Riz Raz",
-    },
-    {
-        id: 2,
-        name: "Stefanos",
-    },
-];
 
 export default function RestaurantsPage() {
-    const [restaurants, setRestaurants] = React.useState([...INIT_DATA]);
     const { name, edited } = useLocalSearchParams<{ name: string; edited: string }>();
     const router = useRouter();
-
     // Returnerer 3 værdier
     const [values, loading, error] = useCollection(collection(database, "restaurants"));
     // Values.docs er dine restaurants, men den har ikke Id på. Dem skal vi sætte på manduelt fra firebase.
-    const data = values?.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as {name: string, id: string}[];
+    const data = values?.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as IRestaurant[];
+    
 
     useEffect(() => {
         // Hvis edited og name er defineret, så skal usestate hooken opdateres
         //! Nedenstående if er fordi vi laver ændringen lokalt. Normalt ville du ændre i db.
-        if (edited && name) {
-            setRestaurants((prev) => prev.map((r) => (r.name === name ? { ...r, name: edited } : r)));
-        }
+        // if (edited && name) {
+        //     setRestaurants((prev) => prev.map((r) => (r.name === name ? { ...r, name: edited } : r)));
+        // }
 
         // Vi sender name tilbage som param fra create.tsx
         // Derfor skal der ske noget hvis name param er defineret her. Usestate skal opdateres og trigger et rerender
@@ -50,12 +35,12 @@ export default function RestaurantsPage() {
                 });
                 return id;
             };
+            
             try {
                 createRestaurant();
             } catch (error) {
                 console.log(error);
             }
-            // setRestaurants((prev) => [...prev, { id: prev.length, name: name }]);
         }
     }, [name, edited]);
 
@@ -68,6 +53,17 @@ export default function RestaurantsPage() {
         router.push(`/restaurants/${name}`);
     };
 
+    const handleDelete = async (id: string) => {
+        // Vi sender navnet på collection og id'et afsted
+        await deleteDoc(doc(database, "restaurants", id));
+    }
+
+    const handleUpdate = async (id: string) => {
+        await updateDoc(doc(database, "restaurants", id), {
+            name: "Updated name"
+        })
+    }
+
     return (
         <ScrollView>
             <View className="items-center mt-2">
@@ -75,7 +71,12 @@ export default function RestaurantsPage() {
             </View>
             <View className="gap-y-2 mt-2">
                 {data?.map((r) => (
-                    <RestaurantCard key={r.id} className="mx-2" name={r.name} onPress={handlePress} />
+                    <View className="flex flex-row">
+                        <RestaurantCard key={r.id} className="mx-2 w-72" name={r.name} onPress={handlePress} />
+                        <TouchableOpacity className="flex justify-center items-center" onPress={() => handleDelete(r.id)}>
+                            <Text>Delete</Text>
+                        </TouchableOpacity>
+                    </View>
                 ))}
             </View>
         </ScrollView>

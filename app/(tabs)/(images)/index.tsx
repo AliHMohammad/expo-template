281d37acf6de84a10b595d4013310071dea1e779
Ali Mohammad <alihmohammad@hotmail.com>
2024-09-22 21:27:53 +0200

    feat: created upload and download image from firebase storage

--- app/(tabs)/(images)/index.tsx
+++ app/(tabs)/(images)/index.tsx
@@ -1,6 +1,8 @@
 import { Button, Image, View } from "react-native";
 import React from "react";
 import * as ImagePicker from "expo-image-picker";
+import { storage } from "../../../firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export default function ImagesPage() {
     const [imagePath, setImagePath] = React.useState("");
@@ -18,10 +20,35 @@ export default function ImagesPage() {
         setImagePath(result.assets[0].uri)
     } 
 
+    const handleUpload = async () => {
+        const res = await fetch(imagePath);
+        const blob = await res.blob();
+        const storageRef = ref(storage, `image_ali`);
+
+        await uploadBytes(storageRef, blob)
+
+        alert("Image uploaded!");
+        setImagePath("");
+    }
+
+    const handleDownload = async () => {
+        getDownloadURL(ref(storage, `image_ali`))
+            .then((url) => setImagePath(url))
+            .catch(() => alert("Something went wrong downloading image"))
+    }
+
     return (
-        <View className="flex flex-1 justify-center items-center">
-            {imagePath && <Image className="w-72 h-72" source={{ uri: imagePath }} />}
-            <Button onPress={handleClick} title="Pick image" />
+        <View className="flex flex-1 items-center">
+            {imagePath && (
+                <View className="mt-5">
+                    <Image className="w-72 h-72" source={{ uri: imagePath }} />
+                    <Button onPress={handleUpload} title="Upload to Firebase Storage" />
+                </View>
+            )}
+            <View className="flex-1 justify-center">
+                <Button onPress={handleDownload} title="Download Image" />
+                <Button onPress={handleClick} title="Pick image" />
+            </View>
         </View>
     );
 }
